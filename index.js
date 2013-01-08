67ae006dc7bec8072948d5a3c1f40379a0ab4b16
Mirko Kiefer <mail@mirkokiefer.com>
2013-01-08 22:38:51 +0200

    add stream iterator

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
 
+var createStreamIterator = function(readable) {
+  var isReadable = true
+  var hasEnded = false
+
+  readable.on('readable', function() {
+    isReadable = true
+  })
+  readable.on('end', function() {
+    hasEnded = true
+  })
+
+  var next = function(cb) {
+    if (hasEnded) {
+      return cb(null, undefined)
+    }
+    if (isReadable) {
+      var res = readable.read()
+      if (res === null) {
+        isReadable = false
+        return next(cb)
+      }
+      cb(null, res)
+    } else {
+      var onEnd = function() { next(cb) }
+      readable.once('readable', function() {
+        readable.removeListener('end', onEnd)
+        next(cb)
+      })
+      readable.once('end', onEnd)
+    }
+  }
+
+  return {next: next}
+}
+
 var forEach = function(iterator, fn, cb) {
   iterator.next(function(err, res) {
     if (res === undefined) return cb(err)
@@ -38,6 +73,7 @@ var mapAsync = function(iterator, fn, cb) {
 }
 
 module.exports = {
+  createStreamIterator: createStreamIterator,
   forEach: forEach,
   forEachAsync: forEachAsync,
   map: map,
--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 
 var assert = require('assert')
 var iterators = require('./index')
+var stream = require('stream')
 
 var data = [1, 2, 3, 4, 5]
 
-var mockAsyncIterator = function() {
+var createMockAsyncIterator = function() {
   var index = -1
   var next = function(cb) {
     setTimeout(function() {
@@ -15,20 +16,37 @@ var mockAsyncIterator = function() {
   return {next: next}
 }
 
+var createMockStream = function() {
+  var mockStream = stream.Readable({objectMode: true, highWaterMark: 2})
+  var index = 0
+
+  mockStream._read = function() {
+    mockStream.push(data[index])
+    index++
+    if (index == 5) mockStream.push(null)
+  }
+
+  return mockStream
+}
+
+var runForEachIteratorTest = function(iterator, cb) {
+  var index = 0
+  iterators.forEach(iterator, function(err, res) {
+    assert.equal(res, data[index])
+    index++
+  }, function() {
+    assert.equal(index, 5)
+    cb()
+  })
+}
+
 describe('async-iterators', function() {
   it('should run forEach', function(done) {
-    var iterator = mockAsyncIterator()
-    var index = 0
-    iterators.forEach(iterator, function(err, res) {
-      assert.equal(res, data[index])
-      index++
-    }, function() {
-      assert.equal(index, 5)
-      done()
-    })
+    var iterator = createMockAsyncIterator()
+    runForEachIteratorTest(iterator, done)
   })
   it('should run forEachAsync', function(done) {
-    var iterator = mockAsyncIterator()
+    var iterator = createMockAsyncIterator()
     var index = 0
     iterators.forEachAsync(iterator, function(err, res, cb) {
       assert.equal(res, data[index])
@@ -40,7 +58,7 @@ describe('async-iterators', function() {
     })
   })
   it('should run map', function(done) {
-    var iterator = mockAsyncIterator()
+    var iterator = createMockAsyncIterator()
     iterators.map(iterator, function(err, res) {
       return res * 2
     }, function(err, res) {
@@ -51,7 +69,7 @@ describe('async-iterators', function() {
     })
   })
   it('should run mapAsync', function(done) {
-    var iterator = mockAsyncIterator()
+    var iterator = createMockAsyncIterator()
     iterators.mapAsync(iterator, function(err, res, cb) {
       cb(null, res * 2)
     }, function(err, res) {
@@ -60,5 +78,10 @@ describe('async-iterators', function() {
       })
       done()
     })
+  })
+  it('should transform a stream into an iterator', function(done) {
+    var mockStream = createMockStream()
+    var mockStreamIterator = iterators.createStreamIterator(mockStream)
+    runForEachIteratorTest(mockStreamIterator, done)
   })
 })
