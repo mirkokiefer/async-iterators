7ca4d4dfcd3ff4face24bb6095f7d4955cb18fc8
Mirko Kiefer <mail@mirkokiefer.com>
2013-08-25 19:53:50 +0200

    simplify tests

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ var createMockAsyncIterator = function() {
     setTimeout(function() {
       index++
       if (!data[index]) return cb(null, undefined)
-      cb(null, {data: data[index], index: index})
+      cb(null, data[index])
     }, 1)
   }
   return {next: next}
@@ -19,8 +19,7 @@ var createMockAsyncIterator = function() {
 var runForEachIteratorTest = function(iterator, cb) {
   var index = 0
   iterators.forEach(iterator, function(err, each) {
-    assert.equal(each.data, data[index])
-    assert.equal(each.index, index)
+    assert.equal(each, data[index])
     index++
   }, function() {
     assert.equal(index, 5)
@@ -37,8 +36,7 @@ describe('async-iterators', function() {
     var iterator = createMockAsyncIterator()
     var index = 0
     iterators.forEachAsync(iterator, function(err, each, cb) {
-      assert.equal(each.data, data[index])
-      assert.equal(each.index, index)
+      assert.equal(each, data[index])
       index++
       cb()
     }, function() {
@@ -47,12 +45,9 @@ describe('async-iterators', function() {
     })
   })
   it('should run map', function(done) {
-    var index = 0
     var iterator = createMockAsyncIterator()
     iterators.map(iterator, function(err, each) {
-      assert.equal(each.index, index)
-      index++
-      return each.data * 2
+      return each * 2
     }, function(err, res) {
       res.forEach(function(each, i) {
         assert.equal(each, data[i] * 2)
@@ -61,12 +56,9 @@ describe('async-iterators', function() {
     })
   })
   it('should run mapAsync', function(done) {
-    var index = 0
     var iterator = createMockAsyncIterator()
     iterators.mapAsync(iterator, function(err, each, cb) {
-      assert.equal(each.index, index)
-      index++
-      cb(null, each.data * 2)
+      cb(null, each * 2)
     }, function(err, res) {
       res.forEach(function(each, i) {
         assert.equal(each, data[i] * 2)
